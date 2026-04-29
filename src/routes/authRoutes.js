685fd22b339ea79da67261a6932829bb95c0b220
João Pedro de Lima <jpjpjplima@gmail.com>
2026-04-28 22:45:27 -0300

    fix: implementa sistema de auth e reset de senha

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { updatePassword } from '../controllers/authController.js';
-import { authMiddleware } from '../middleware/auth.js';
+import { authMiddleware } from '../middlewares/auth.js';
 
 const router = express.Router();
 
